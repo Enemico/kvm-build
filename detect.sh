ae199da936df31aecab06239a7d5e31e3b3dbd52
Enrico Razzetti <sub@krutt.org>
2017-05-03 13:21:39 +0200

    added check for root also on detect, because of arp command

--- detect.sh
+++ detect.sh
@@ -8,6 +8,13 @@ usage () {
   echo "Usage: $0 [instancename]"
 }
 
+### Am i Root check
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root, or preceded by sudo."
+  echo "If sudo does not work, contact your system administrator."
+  exit 1
+fi
+
 if [ -z $1 ]; then
   echo "Provide an instance name, pretty please?"
   usage
