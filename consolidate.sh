da30a4f7f7715048b1b7989363f854f31a05e4f1
Enrico Razzetti <sub@krutt.org>
2017-05-03 13:16:31 +0200

    added consolidate, adapted to a default ubuntu installation

---
+++ consolidate.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+VM=$1
+VIRSH=/usr/bin/virsh
+QEMU=/usr/bin/qemu-img
+POOL=default
+POOL_DIR=/var/lib/libvirt/images
+
+usage () {
+  echo "This command consolidates the storage of a previously created clone."
+  echo "It truns off the vm (if running), and then merges a copy "
+  echo "of the golden image with the snapshot used by the clone, to obtain"
+  echo "a standalone image for the affected vm."
+  echo "Usage: $0 [instancename]"
+}
+
+if [ -z $1 ]; then
+  echo "Provide an instance name, pretty please?"
+  usage
+  exit 1
+fi
+
+if [ ! -f /etc/libvirt/qemu/${VM}.xml ]; then
+  echo "$VM doesn't exists, as far as i can see."
+  exit 1
+fi
+
+check_running () {
+  $VIRSH list --name | grep -w $VM > /dev/null 2>&1
+}
+
+pool_list () {
+  $VIRSH vol-list --pool $POOL
+}
+
+check_running
+if [ $? -eq "0" ]; then
+  echo "The requested instance $VM it's running on this hypervisor."
+  echo "We will have to turn it down."
+  $VIRSH shutdown ${VM}
+  sleep 10
+  check_running
+  if [ $? -eq "0" ]; then
+    echo "$VM is still running, i will terminate it now."
+    $VIRSH destroy ${VM}
+    check_running
+    if [ $? -eq "0" ]; then
+      echo "$VM is still alive after destroy, something is fishy..."
+      echo "Bailing out"
+      exit 1
+    fi
+  else
+    echo "The domain ${VM} has been shutdown, continuing..."
+  fi
+else
+  echo "the requested instance $VM it's not running, good."
+  echo "Continuing..."
+fi
+
+check_running
+if [ $? -eq "0" ]; then
+  echo "$VM is still running, and we previously tried to destroy it. Bailing out."
+  exit 1
+fi
+
+### From the instance name, extract the DISTRO name.
+DISTRO=$($VIRSH list --inactive --name | grep ${VM} | cut -f1 -d ".")
+echo "${VM} is using the golden image for the $DISTRO distribution."
+
+### Make a copy of the golden image of the corresponding distribution.
+pool_list | grep $DISTRO.golden.img > /dev/null 2>&1
+if [ $? -ne "0" ]; then
+  echo "Uhm, something is wrong, i can't find the corresponding golden image, for some reason."
+  echo "Bailing out."
+  exit 1
+fi
+
+echo "Copying the golden image to a temporary file."
+$VIRSH vol-clone --pool $POOL $DISTRO.golden.img ${VM}.base.img
+pool_list | grep ${VM}.base.img
+if [ $? -ne "0" ]; then
+  echo "Something went wrong with the cloning of the golden image."
+  echo "Bailing out."
+  exit 1
+fi
+
+echo "Here is the backing chain for ${VM} atm."
+## Check the backing chain
+$QEMU info --backing-chain $POOL_DIR/${VM}.qcow2
+
+
+## Rebasing of the image.
+#$QEMU rebase -b $POOL_DIR/$POOL/${VM}.base.img $POOL_DIR/$POOL/${VM}.qcow2
+#$QEMU commit $POOL_DIR/$POOL/${VM}.qcow2
+
+$QEMU rebase -b $POOL_DIR/${VM}.base.img $POOL_DIR/${VM}.qcow2
+$QEMU commit $POOL_DIR/${VM}.qcow2
+
+
+## Removing the clone image
+rm -f $POOL_DIR/${VM}.qcow2
+
+## Renaming the rebased image wuth the VM name
+mv $POOL_DIR/${VM}.base.img $POOL_DIR/${VM}.qcow2
+/bin/chown qemu:qemu $POOL_DIR/${VM}.qcow2
+chmod +w $POOL_DIR/${VM}.qcow2
+
+echo "Here is the new backing chain for $VM after the consolidation (rebase ) process."
+
+## Check the backing chain
+$QEMU info --backing-chain $POOL_DIR/${VM}.qcow2
+
+## fix permissions
+chown libvirt-qemu:kvm $POOL_DIR/${VM}.qcow2
+chmod 644 $POOL_DIR/${VM}.qcow2
+
+
+echo "If this looks OK, then you can run: virsh start $VM"
+
+
+exit 0
+
